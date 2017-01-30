fc738dc32a6ff0e386deaef568dc6da2c94a485a
alwaysSmile <sjuice@mail.ru>
2017-01-30 07:30:04 +0300

    get files from npm project

---
+++ petya/index.js
@@ -0,0 +1,10 @@
+//Петя получает supermodule
+//Мы находимся в папке petya, соотвественно в неё и установится модуль
+//Команды в cmd:
+//npm search dfmodule или npm s super module (dfmodule - имя модуля) - находим модуль
+//npm install dfmodule или npm i dfmodule - устанавливаем модуль
+//npm update или npm up - обновить модуль
+//npm remove dfmodule или npm r dfmodule - удалить модуль
+
+var supermodule = require('dfmodule');
+supermodule();
---
+++ petya/node_modules/dfmodule/index.js
@@ -0,0 +1,25 @@
+// Изучаем NPM
+//package.json мы можем создать IDE или в
+//cmd написать (в данном случае)
+//cd WebstormProjects - зашли в папку WebstormProjects
+//cd npm - зашли в папку npm
+//cd npm - зашли в папку npm
+//cd supermodule - зашли в папку supermodule и в ней в cmd пишем
+//npm init
+
+//После того, когда создан модуль package.json его нужно опубликовать
+//в центральной базе данных - npm publish (если на текущий момент вызвать - будет ошибка, стобы
+//опубликовать нужен user)
+
+//!!!В файле package.json ключ name, должен иметь имя, какое ты укажешь {"name": "vasya"}
+//npm adduser - добавить user
+//Username: 21
+//Password: 123456m
+//Public Email: sjuice@mail.ru
+
+//npm help - команды
+
+module.exports = function() {
+    console.log('Этот модуль - самый лучший модуль на земле');
+    console.log('Помогает рисовать он мелом на стене');
+};
